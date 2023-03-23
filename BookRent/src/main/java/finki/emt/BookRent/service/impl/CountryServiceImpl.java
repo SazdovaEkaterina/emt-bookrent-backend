a6582d492727f942dd3719d52134053a72283e54
SazdovaEkaterina <sazdovaekaterina@gmail.com>
2023-03-23 11:32:35 +0100

    country rest controller

--- BookRent/src/main/java/finki/emt/BookRent/service/impl/CountryServiceImpl.java
+++ BookRent/src/main/java/finki/emt/BookRent/service/impl/CountryServiceImpl.java
@@ -1,8 +1,15 @@
 package finki.emt.BookRent.service.impl;
 
+import finki.emt.BookRent.model.Country;
+import finki.emt.BookRent.model.dto.CountryDto;
+import finki.emt.BookRent.model.exceptions.BookNotFoundException;
+import finki.emt.BookRent.model.exceptions.CountryNotFoundException;
 import finki.emt.BookRent.repository.CountryRepository;
 import finki.emt.BookRent.service.CountryService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryServiceImpl implements CountryService {
@@ -13,4 +20,40 @@ public class CountryServiceImpl implements CountryService {
         this.countryRepository = countryRepository;
     }
 
+    @Override
+    public List<Country> findAll() {
+        return this.countryRepository.findAll();
+    }
+
+    @Override
+    public Optional<Country> findById(Long id) {
+        return this.countryRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Country> save(CountryDto countryDto) {
+
+        this.countryRepository.deleteByName(countryDto.getName());
+        Country country = new Country(countryDto.getName(), countryDto.getContinent());
+        this.countryRepository.save(country);
+        return Optional.of(country);
+
+    }
+
+    @Override
+    public Optional<Country> edit(Long id, CountryDto countryDto) {
+
+        Country country = this.countryRepository.findById(id).orElseThrow(() -> new CountryNotFoundException(id));
+        country.setName(countryDto.getName());
+        country.setContinent(countryDto.getContinent());
+        this.countryRepository.save(country);
+
+        return Optional.of(country);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.countryRepository.deleteById(id);
+    }
 }
--- BookRent/src/main/java/finki/emt/BookRent/web/rest/CountryRestController.java
+++ BookRent/src/main/java/finki/emt/BookRent/web/rest/CountryRestController.java
@@ -1,9 +1,59 @@
 package finki.emt.BookRent.web.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import finki.emt.BookRent.model.Book;
+import finki.emt.BookRent.model.Country;
+import finki.emt.BookRent.model.dto.BookDto;
+import finki.emt.BookRent.model.dto.CountryDto;
+import finki.emt.BookRent.service.CountryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/countries")
 public class CountryRestController {
+
+    private final CountryService countryService;
+
+    public CountryRestController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Country> save(@RequestBody CountryDto countryDto) {
+        return this.countryService.save(countryDto)
+                .map(country -> ResponseEntity.ok().body(country))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @GetMapping
+    public List<Country> findAll() {
+        return this.countryService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> findById(@PathVariable Long id) {
+        return this.countryService.findById(id)
+                .map(country -> ResponseEntity.ok().body(country))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}/edit")
+    public ResponseEntity<Country> edit(@PathVariable Long id,
+                                        @RequestBody CountryDto countryDto) {
+        return this.countryService.edit(id, countryDto)
+                .map(book -> ResponseEntity.ok().body(book))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity delete(@PathVariable Long id) {
+        this.countryService.deleteById(id);
+        if (this.countryService.findById(id).isEmpty()) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
 }
