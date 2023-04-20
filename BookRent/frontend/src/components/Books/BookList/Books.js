0e5b758ee10762f203d557d3c6192cf5399d7855
SazdovaEkaterina <sazdovaekaterina@gmail.com>
2023-04-20 12:42:19 +0200

    pagination

--- BookRent/frontend/src/components/Books/BookList/Books.js
+++ BookRent/frontend/src/components/Books/BookList/Books.js
@@ -1,42 +1,92 @@
-import React from "react";
+import React, {Component} from "react";
 import BookItem from "../BookItem/BookItem";
+import ReactPaginate from 'react-paginate'
 
-const Books = (props) => {
-    return (
-        <div className={"container mm-4 mt-5"}>
-            <div className={"row"}>
-                <div className="col mb-3">
-                    <div className="row">
-                        <div className="col-sm-12 col-md-12">
-                            <a className="btn btn-block btn-dark" href="/books/add">+ Add new book</a>
+class Books extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            page: 0,
+            size: 5
+        }
+    }
+
+    render(){
+
+        const offset = this.state.size * this.state.page;
+        const nextPageOffset = offset + this.state.size;
+        const pageCount = Math.ceil(this.props.books.length / this.state.size);
+        const books = this.getBooksPage();
+        console.log(books, pageCount)
+
+        return (
+            <div className={"container mm-4 mt-5"}>
+                <div className={"row"}>
+                    <div className="col mb-3">
+                        <div className="row">
+                            <div className="col-sm-12 col-md-12">
+                                <a className="btn btn-block btn-dark" href="/books/add">+ Add new book</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className={"table-responsive"}>
-                    <table className={"table table-striped"}>
-                        <thead>
-                        <tr>
-                            <th scope={"col"}>Name</th>
-                            <th scope={"col"}>Category</th>
-                            <th scope={"col"}>Author</th>
-                            <th scope={"col"}>Available copies</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {props.books.map((book:B) => {
-                            return (
-                                <BookItem book={book}
-                                          onDelete={props.onDelete}
-                                          onEdit={props.onEdit}
-                                          onMarkAsTaken={props.onMarkAsTaken}/>
-                            );
-                        })}
-                        </tbody>
-                    </table>
+                    <div className={"table-responsive"}>
+                        <table className={"table table-striped"}>
+                            <thead>
+                            <tr>
+                                <th scope={"col"}>Name</th>
+                                <th scope={"col"}>Category</th>
+                                <th scope={"col"}>Author</th>
+                                <th scope={"col"}>Available copies</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                {books}
+                            </tbody>
+                            <ReactPaginate previousLabel={"back"}
+                                           nextLabel={"next"}
+                                           breakLabel={<a href="/#">...</a>}
+                                           breakClassName={"break-me"}
+                                           pageClassName={"ml-1"}
+                                           pageCount={pageCount}
+                                           marginPagesDisplayed={2}
+                                           pageRangeDisplayed={5}
+                                           onPageChange={this.handlePageClick}
+                                           containerClassName={"pagination m-4 justify-content-center"}
+                                           activeClassName={"active"}/>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    handlePageClick = (data) => {
+        let selected = data.selected;
+        console.log(selected)
+        this.setState({
+            page: selected
+        })
+    }
+
+    getBooksPage = (offset, nextPageOffset) => {
+        console.log(offset, nextPageOffset)
+        return this.props.books.map((book, index) => {
+            return (
+                <BookItem book={book}
+                          onDelete={this.props.onDelete}
+                          onEdit={this.props.onEdit}
+                          onMarkAsTaken={this.props.onMarkAsTaken}/>
+            );
+        })
+        //     .filter((book, index) => {
+        //     return index >=offset && index < nextPageOffset;
+        // })
+    }
+
 }
 
 export default Books;
